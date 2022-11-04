9cdfb5f0be3ccddc5b6fa3e9ca07abe3cb3022fd
米苏 <zhoux0130@hotmail.com>
2022-11-04 18:52:54 +0800

    feat： import supertest

--- src/tests/handlers/user_spec.ts
+++ src/tests/handlers/user_spec.ts
@@ -1,26 +1,26 @@
-// import supertest from "supertest";
-// import app from '../../server';
-// import { User } from '../../models/user';
-// import jwt from 'jsonwebtoken';
-// import dotenv from 'dotenv';
-// import { idText } from "typescript";
+import supertest from "supertest";
+import app from '../../server';
+import { User } from '../../models/user';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { idText } from "typescript";
 
-// dotenv.config();
-// const pepper: string = process.env.BCRYPT_PW as string;
+dotenv.config();
+const pepper: string = process.env.BCRYPT_PW as string;
 
-// const request = supertest(app);
+const request = supertest(app);
 
-// describe("testing /users endpoint", () => {
-//   const lilyChoo: User = {
-//     firstName: 'Lily',
-//     lastName: 'Choo',
-//     password: '123'
-//   }
+describe("testing /users endpoint", () => {
+  const lilyChoo: User = {
+    firstName: 'Lily',
+    lastName: 'Choo',
+    password: '123'
+  }
 
-//   let tokenLily: string;
+  let tokenLily: string;
   
-//   it("should create user Lily", async () => {
-//     const response = await request.post('/users').send(lilyChoo).expect(200)
-//   })
+  it("should create user Lily", async () => {
+    const response = await request.post('/users').send(lilyChoo).expect(200)
+  })
 
-// })
+})
