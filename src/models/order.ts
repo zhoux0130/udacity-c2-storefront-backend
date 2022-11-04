0702a8ea31e8a865c9b70b3292d9d4c432c5c1a7
米苏 <zhoux0130@hotmail.com>
2022-11-04 19:49:35 +0800

    feat： model test finish

--- src/models/order.ts
+++ src/models/order.ts
@@ -4,13 +4,38 @@ export type Order = {
   id?: number,
   quantity: number,
   status: string,
-  user_id: number,
-  product_id: number
+  userId: number,
+  productId: number
 }
 
 export class OrderStore {
-  // async getOrders(userId: string): Promise<Order[]> {
+  async getUserOrders(userId: string): Promise<Order[]> {
+    try {
+      const conn = await Client.connect();
+      const sql = `SELECT * FROM orders WHERE user_id=${userId};`;
 
-  // }
+      const result = await conn.query(sql);
+
+      conn.release()
+      return result.rows
+    } catch (err) {
+        throw new Error(`Could not find order ${userId}. Error: ${err}`)
+    }
+  }
+
+  async create(order: Order): Promise<Order> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'INSERT INTO orders (user_id, status, product_id, quantity) VALUES($1, $2, $3, $4) RETURNING *;';
+      
+      const result = await conn
+          .query(sql, [order.userId, order.status, order.productId, order.quantity]);
+      
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+        throw new Error(`Could not add new order from user ${order.userId}. Error: ${err}`);
+    }
+  }
 }
 
---
+++ src/tests/models/order_spec.ts
@@ -0,0 +1,44 @@
+import { Order, OrderStore } from '../../models/order'
+import { ProductStore } from '../../models/product';
+import { UserStore } from '../../models/user';
+
+const store = new OrderStore()
+const userStore = new UserStore()
+const productStore = new ProductStore()
+
+let testUserId: number;
+
+describe('Order Store Model', () => {
+  it('should have an getUserOrders method', async () => {
+    const user = {
+      firstName:'Lily',
+      lastName:'Choo',
+      password:'123'
+    }
+    const userResult = await userStore.create(user);
+    testUserId = userResult.id!;
+
+    const product = {
+      name: 'Apple',
+      price: 0.59,
+      category: 4
+    }
+    const productResut = await productStore.create(product);
+    const productId:number = productResut.id!;
+
+    const order: Order = {
+      quantity: 1,
+      status: 'active',
+      userId: testUserId,
+      productId
+    }
+    await store.create(order)
+    expect(store.getUserOrders).toBeDefined();
+
+  });
+
+  it('should get test users orders', async () => {
+    const orders = await store.getUserOrders(testUserId.toString());
+    expect(orders).toHaveSize;
+  });
+})
